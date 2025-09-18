0c1ec84e583eec3785376beca2125a1283ea83e6
Minh Đức Chử <duccm.tomotek@gmail.com>
2025-09-18 11:07:42 +0700

    feat(auth): Add Session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(session({
+    name: 'NESTJS_SESSION_ID',
+    secret: 'KDSFGSDKHGDKFAKSDPWEFMLEMFMDSFNDJGBSDMSLKFSD',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000,
+    },
+  }));
   await app.listen(process.env.PORT ?? 5001);
 }
 bootstrap();
