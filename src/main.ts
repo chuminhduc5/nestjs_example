a0c9951392842f241cc291676c4393ea01b7c109
Minh Đức Chử <duccm.tomotek@gmail.com>
2025-09-20 23:23:58 +0700

    feat(auth): Update Session Stores

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import session from 'express-session';
 import passport from 'passport';
+import { TypeormStore } from 'connect-typeorm';
+import { DataSource } from 'typeorm';
+import { SessionEntity } from './typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const dataSource = app.get(DataSource);
+  const sessionRepository = dataSource.getRepository(SessionEntity);
   app.setGlobalPrefix('api');
   app.use(session({
     name: 'NESTJS_SESSION_ID',
@@ -14,6 +19,7 @@ async function bootstrap() {
     cookie: {
       maxAge: 60000,
     },
+    store: new TypeormStore().connect(sessionRepository),
   }));
   app.use(passport.initialize());
   app.use(passport.session());
---
+++ src/typeorm/Session.ts
@@ -0,0 +1,18 @@
+import { ISession } from "connect-typeorm";
+import { Column, DeleteDateColumn, Entity, Index, PrimaryColumn } from "typeorm";
+
+@Entity({ name: 'sessions' })
+export class SessionEntity implements ISession {
+    @Index()
+    @Column('bigint')
+    expiredAt = Date.now();
+
+    @PrimaryColumn('varchar', { length: 255 })
+    id = '';
+    
+    @Column('text')
+    json = '';
+
+    @DeleteDateColumn()
+    destroyedAt?: Date | undefined;
+}
